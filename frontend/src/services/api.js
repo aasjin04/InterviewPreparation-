72c60ba46c318b9e4859c15beec62496ddc84df7
Aastha Jindal <aasthajindal08@gmail.com>
2026-09-11 22:41:31 +0530

    Prepare project for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((config) => {
@@ -15,4 +15,3 @@ API.interceptors.request.use((config) => {
 });
 
 export default API;
-// har tool pe go to dashboard in right side and explore more tools jo redirect ho home k tools vle page pe or isko in left side plus login or sign up page pe bhi back to home ka option nhi h
